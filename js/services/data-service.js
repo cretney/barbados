a1d08ecbc3639e20232195b63aadc736b35a5730
Joe Hogan <joseph.t.hogan@gmail.com>
2015-09-24 20:32:12 -0400

    updated to working concept including POST service

--- js/services/data-service.js
+++ js/services/data-service.js
@@ -2,16 +2,31 @@
 
 	var dataService = angular.module('dataService', [])
 
-	.factory('dataService', ['$q','$http', function($q,$http) {
+	.factory('dataService', ['$filter','$http','$q', function($filter,$http,$q) {
 		return {
 			getForm: function(id){
 				return $http.get("http://52.25.174.100/Forms/GetForm/"+id);
+				/*
+				var def = $q.defer();  //testing with local data
+				this.getForms().then(function(forms){
+					forms.data = $filter('filter')(forms.data,{blockId: parseInt(id)},true)[0];
+					def.resolve(forms);
+				});
+				return def.promise;
+				*/
 			},
 			getForms: function(){
 				return $http.get("http://52.25.174.100/Forms/GetFormData");
+				//return $http.get("sample-data/forms.json", {cache: true}); //testing with local data
 			},
-			getLists: function(){
-				return $http.get("http://52.25.174.100/Forms/GetLists", {cache: true});
+			postForm: function(data,id){
+				if(id && !data.blockId) data.blockId = id;
+				return $http.post("http://52.25.174.100/Forms/PostForm",{formData: JSON.stringify(data)});
+			},
+			getLists: function(id,types){
+				var t = types || ['country','unitsOfMeasure','suppliers','reason','comment','recommendation','condition','organisations','formsMap','approvalCodes'];
+				return $http.get("http://52.25.174.100/Forms/GetLists?blockId="+id+"&listTypes="+t.join(','), {cache: true});
+				//return $http.get("sample-data/lists.json", {cache: true}); //testing with local data
 			}
 		}
 	}]);
