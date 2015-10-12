c38c5fd1cdfc70ff5601e58b41ba8e777050141b
Joe Hogan <joseph.t.hogan@gmail.com>
2015-10-12 11:22:17 -0400

    removed all sample data to avoid confusion

--- js/services/data-service.js
+++ js/services/data-service.js
@@ -6,19 +6,9 @@
 		return {
 			getForm: function(id){
 				return $http.get("http://52.25.174.100/Forms/GetForm/"+id);
-				/*
-				var def = $q.defer();  //testing with local data
-				this.getForms().then(function(forms){
-					forms.data = $filter('filter')(forms.data,{blockId: parseInt(id)},true)[0];
-					def.resolve(forms);
-				});
-				return def.promise;
-				*/
-				
 			},
 			getForms: function(){
 				return $http.get("http://52.25.174.100/Forms/GetFormData");
-				//return $http.get("sample-data/forms.json", {cache: true}); //testing with local data
 			},
 			postForm: function(data,id){
 				if(id && !data.blockId) data.blockId = id;
@@ -27,7 +17,6 @@
 			getLists: function(id,types){
 				var t = types || ['country','unitsOfMeasure','suppliers','reason','comment','recommendation','condition','organisations','formsMap','approvalCodes'];
 				return $http.get("http://52.25.174.100/Forms/GetLists?blockId="+id+"&listTypes="+t.join(','), {cache: true});
-				//return $http.get("sample-data/lists.json", {cache: true}); //testing with local data
 			}
 		}
 	}]);
